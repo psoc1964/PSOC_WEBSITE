a1c80290197ef74a98cccbe221869d0704bfede4
Abhinaw-47 <abhi11naw@gmail.com>
2025-03-17 19:56:23 +0530

    installed the dependencies and backend setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import './index.css'
 import './App.css'
 
 function App() {
@@ -7,7 +7,9 @@ function App() {
 
   return (
     <>
-      
+    <h1 className="text-3xl font-bold underline bg-red-400">
+      Hello world!
+    </h1>
     </>
   )
 }
